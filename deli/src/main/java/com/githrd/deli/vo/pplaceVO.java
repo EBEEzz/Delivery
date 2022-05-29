22d8240129d2a928ac27a32e14f17b847e1e7b87
USER <USER@DESKTOP-C4V5UFU>
2022-05-29 19:29:15 +0900

    0529 commit (package경로 수정)

--- deli/src/main/java/com/githrd/deli/vo/pplaceVO.java
+++ deli/src/main/java/com/githrd/deli/vo/pplaceVO.java
@@ -1,56 +1,69 @@
-package vo;
+package com.githrd.deli.vo;
 
 public class pplaceVO {
 	private String cusname, cusid;
-	private double pickuplat, pickuplon, cus_lat,cus_lon,distance;
+	private double pickuplat, pickuplon, cus_lat, cus_lon, distance;
+
 	public String getCusname() {
 		return cusname;
 	}
+
 	public void setCusname(String cusname) {
 		this.cusname = cusname;
 	}
+
 	public String getCusid() {
 		return cusid;
 	}
+
 	public void setCusid(String cusid) {
 		this.cusid = cusid;
 	}
+
 	public double getPickuplat() {
 		return pickuplat;
 	}
+
 	public void setPickuplat(double pickuplat) {
 		this.pickuplat = pickuplat;
 	}
+
 	public double getPickuplon() {
 		return pickuplon;
 	}
+
 	public void setPickuplon(double pickuplon) {
 		this.pickuplon = pickuplon;
 	}
+
 	public double getCus_lat() {
 		return cus_lat;
 	}
+
 	public void setCus_lat(double cus_lat) {
 		this.cus_lat = cus_lat;
 	}
+
 	public double getCus_lon() {
 		return cus_lon;
 	}
+
 	public void setCus_lon(double cus_lon) {
 		this.cus_lon = cus_lon;
 	}
+
 	public double getDistance() {
 		return distance;
 	}
+
 	public void setDistance(double distance) {
 		this.distance = distance;
 	}
+
 	@Override
 	public String toString() {
 		return "pplaceVO [cusname=" + cusname + ", cusid=" + cusid + ", pickuplat=" + pickuplat + ", pickuplon="
 				+ pickuplon + ", cus_lat=" + cus_lat + ", cus_lon=" + cus_lon + ", distance=" + distance + "]";
 	}
-	
-	
-	
+
 }
--- deli/src/main/java/com/githrd/deli/vo/rcoordiVO.java
+++ deli/src/main/java/com/githrd/deli/vo/rcoordiVO.java
@@ -1,74 +1,87 @@
-package vo;
+package com.githrd.deli.vo;
 
 public class rcoordiVO {
-	private int resno,cate_no;
-	private String resname,category;
+	private int resno, cate_no;
+	private String resname, category;
 	private double reslat, reslon, mylat, mylon, resdist;
-	
+
 	public String getCategory() {
 		return category;
 	}
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
 	public int getCate_no() {
 		return cate_no;
 	}
+
 	public void setCate_no(int cate_no) {
 		this.cate_no = cate_no;
 	}
+
 	public int getResno() {
 		return resno;
 	}
+
 	public void setResno(int resno) {
 		this.resno = resno;
 	}
+
 	public String getResname() {
 		return resname;
 	}
+
 	public void setResname(String resname) {
 		this.resname = resname;
 	}
+
 	public double getReslat() {
 		return reslat;
 	}
+
 	public void setReslat(double reslat) {
 		this.reslat = reslat;
 	}
+
 	public double getReslon() {
 		return reslon;
 	}
+
 	public void setReslon(double reslon) {
 		this.reslon = reslon;
 	}
+
 	public double getMylat() {
 		return mylat;
 	}
+
 	public void setMylat(double mylat) {
 		this.mylat = mylat;
 	}
+
 	public double getMylon() {
 		return mylon;
 	}
+
 	public void setMylon(double mylon) {
 		this.mylon = mylon;
 	}
+
 	public double getResdist() {
 		return resdist;
 	}
+
 	public void setResdist(double resdist) {
 		this.resdist = resdist;
 	}
+
 	@Override
 	public String toString() {
 		return "rcoordiVO [resno=" + resno + ", resname=" + resname + ", category=" + category + ", reslat=" + reslat
 				+ ", reslon=" + reslon + ", mylat=" + mylat + ", mylon=" + mylon + ", resdist=" + resdist + ", cate_no="
 				+ cate_no + "]";
 	}
-	
-	
-	
-	
-	
-	
+
 }
